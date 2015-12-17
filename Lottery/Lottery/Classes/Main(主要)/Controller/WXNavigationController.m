a3cf3206a5e8594012617a2e67afe7c9a111b866
李伟雄 <liwx@localhost>
2015-12-17 23:05:01 +0800

    1.通过修改导航条的主题颜色为白色,并将返回按钮的文字移除屏幕.实现返回按钮为白色的箭头

--- Lottery/Lottery/Classes/Main(主要)/Controller/WXNavigationController.m
+++ Lottery/Lottery/Classes/Main(主要)/Controller/WXNavigationController.m
@@ -23,34 +23,44 @@
 {
     if (self == [WXNavigationController class]) {
         
-//        // 方式一
-//        // 一次性设置导航条的样式
-//        // 设置程序所有导航条样式,应用程序所有导航条都会改变
-//        // UINavigationBar *navBar = [UINavigationBar appearance];
-//
-//        // 取出当前导航控制器的导航条appearanceWhenContainedIn:方法已过期
-//        UINavigationBar *navBar = [UINavigationBar appearanceWhenContainedIn:self, nil];
-//        
-//        // 设置导航条的背景图片
-//        [navBar setBackgroundImage:[UIImage imageNamed:@"NavBar64"] forBarMetrics:UIBarMetricsDefault];
-//        
-//        // 设置导航控制器的导航条的标题样式
-//        NSDictionary *attrDict = @{
-//                                   NSFontAttributeName : [UIFont boldSystemFontOfSize:20],
-//                                   NSForegroundColorAttributeName : [UIColor whiteColor]
-//                                   };
-//        [navBar setTitleTextAttributes:attrDict];
-        
-        // 方式二 iOS9新增appearanceWhenContainedInInstancesOfClasses方法
+        // 方式一
         // 一次性设置导航条的样式
-        [[UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[[WXNavigationController class]]] setBackgroundImage:[UIImage imageNamed:@"NavBar64"] forBarMetrics:UIBarMetricsDefault];
+        // 设置程序所有导航条样式,应用程序所有导航条都会改变
+        // UINavigationBar *navBar = [UINavigationBar appearance];
+
+        // 取出当前导航控制器的导航条appearanceWhenContainedIn:方法已过期
+        UINavigationBar *navBar = [UINavigationBar appearanceWhenContainedIn:self, nil];
+        
+        // 设置导航条的背景图片
+        [navBar setBackgroundImage:[UIImage imageNamed:@"NavBar64"] forBarMetrics:UIBarMetricsDefault];
         
         // 设置导航控制器的导航条的标题样式
         NSDictionary *attrDict = @{
                                    NSFontAttributeName : [UIFont boldSystemFontOfSize:20],
                                    NSForegroundColorAttributeName : [UIColor whiteColor]
                                    };
-        [[UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[[WXNavigationController class]]] setTitleTextAttributes:attrDict];
+        [navBar setTitleTextAttributes:attrDict];
+        
+        // 方式二 iOS9新增appearanceWhenContainedInInstancesOfClasses方法
+        // 一次性设置导航条的样式
+//        [[UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[[WXNavigationController class]]] setBackgroundImage:[UIImage imageNamed:@"NavBar64"] forBarMetrics:UIBarMetricsDefault];
+//        
+//        // 设置导航控制器的导航条的标题样式
+//        NSDictionary *attrDict = @{
+//                                   NSFontAttributeName : [UIFont boldSystemFontOfSize:20],
+//                                   NSForegroundColorAttributeName : [UIColor whiteColor]
+//                                   };
+//        [[UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[[WXNavigationController class]]] setTitleTextAttributes:attrDict];
+        
+        // 使用投机小技巧,将导航条的主题改成白色的,并把文字移除屏幕
+        [navBar setTintColor:[UIColor whiteColor]];
+        
+        // 导航条的内容由UIBarButtonItem决定
+        // ???: 不明白以下方法如何获取所有UIBarButtonItem
+        // 获取当前类所有的UIBarButtonItem, 将返回按钮的标题移除显示区域
+        UIBarButtonItem *barButtonItem = [UIBarButtonItem appearanceWhenContainedIn:self, nil];
+        [barButtonItem setBackButtonTitlePositionAdjustment:UIOffsetMake(-1000, 0) forBarMetrics:UIBarMetricsDefault];
+        
     
     }
   
@@ -62,7 +72,8 @@
     // 判断是否是导航控制器的根控制器,如果是根控制器,则不添加导航条左边返回按钮
     if (self.childViewControllers.count != 0) {
         // 设置跳转的目标控制器的返回按钮为白色箭头的图片
-        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageWithOriginalImageName:@"NavBack"] style:UIBarButtonItemStyleDone target:self action:@selector(leftBarButtonClick)];
+        // 以下操作覆盖了导航条系统自带的返回按钮,系统的边缘滑动功能失效,此方法不可行
+//        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageWithOriginalImageName:@"NavBack"] style:UIBarButtonItemStyleDone target:self action:@selector(leftBarButtonClick)];
     }
     
     // 执行跳转操作
