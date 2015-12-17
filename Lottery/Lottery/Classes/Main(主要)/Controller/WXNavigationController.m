92b30d5a660314da1bde72e25a7e5dc18bf3858d
李伟雄 <liwx@localhost>
2015-12-18 00:02:57 +0800

    1.通过设置导航控制器的系统手势代理,解决在导航控制器的根控制器中拖动,会出现假死状态的问题

--- Lottery/Lottery/Classes/Main(主要)/Controller/WXNavigationController.m
+++ Lottery/Lottery/Classes/Main(主要)/Controller/WXNavigationController.m
@@ -9,6 +9,13 @@
 #import "WXNavigationController.h"
 #import "UIImage+Image.h"
 
+@interface WXNavigationController () <UINavigationControllerDelegate>
+
+// 用来记录系统的手势代理
+@property (nonatomic, strong) id popDelegate;
+
+@end
+
 @implementation WXNavigationController
 
 // 当加载类的时候调用,把类加载到内存,存放在代码区
@@ -52,18 +59,46 @@
 //                                   };
 //        [[UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[[WXNavigationController class]]] setTitleTextAttributes:attrDict];
         
+        
+        
         // 使用投机小技巧,将导航条的主题改成白色的,并把文字移除屏幕
-        [navBar setTintColor:[UIColor whiteColor]];
+//        [navBar setTintColor:[UIColor whiteColor]];
         
         // 导航条的内容由UIBarButtonItem决定
         // ???: 不明白以下方法如何获取所有UIBarButtonItem
         // 获取当前类所有的UIBarButtonItem, 将返回按钮的标题移除显示区域
-        UIBarButtonItem *barButtonItem = [UIBarButtonItem appearanceWhenContainedIn:self, nil];
-        [barButtonItem setBackButtonTitlePositionAdjustment:UIOffsetMake(-1000, 0) forBarMetrics:UIBarMetricsDefault];
+//        UIBarButtonItem *barButtonItem = [UIBarButtonItem appearanceWhenContainedIn:self, nil];
+//        [barButtonItem setBackButtonTitlePositionAdjustment:UIOffsetMake(-1000, 0) forBarMetrics:UIBarMetricsDefault];
         
     
     }
   
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // 禁用系统边缘滑动功能
+//    self.interactivePopGestureRecognizer.enabled = NO;
+    // 取消系统手势代理
+//    self.interactivePopGestureRecognizer.delegate = nil;
+    
+    // 成员属性记录系统的手势
+    _popDelegate = self.interactivePopGestureRecognizer.delegate;
+    
+    // 设置导航控制器的代理,目的是监听子控制器有没有完全显示
+    self.delegate = self;
+}
+
+// 当导航控制器的子控制器完全显示调用
+- (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    // 判断是否是导航控制器的根控制器,如果是根控制器,则恢复代理,否则如果在导航控制器的根控制器中拖动,会出现假死状态
+    if (viewController == self.childViewControllers[0]) {
+        // 还原系统手势
+        self.interactivePopGestureRecognizer.delegate = _popDelegate;
+    }
 }
 
 // 重写pushViewController方法,通过该方法实现统一修改导航栏左边的返回按钮
@@ -73,7 +108,10 @@
     if (self.childViewControllers.count != 0) {
         // 设置跳转的目标控制器的返回按钮为白色箭头的图片
         // 以下操作覆盖了导航条系统自带的返回按钮,系统的边缘滑动功能失效,此方法不可行
-//        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageWithOriginalImageName:@"NavBack"] style:UIBarButtonItemStyleDone target:self action:@selector(leftBarButtonClick)];
+        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageWithOriginalImageName:@"NavBack"] style:UIBarButtonItemStyleDone target:self action:@selector(leftBarButtonClick)];
+        
+        // 如果是根控制器,取消系统手势代理
+        self.interactivePopGestureRecognizer.delegate = nil;
     }
     
     // 执行跳转操作
