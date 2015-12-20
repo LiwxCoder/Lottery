72100537eb7962bddb6e302672dced04bee68d9e
liwx199105 <1032282633@qq.com>
2015-12-20 17:01:13 +0800

    1.添加发现界面动画

--- Lottery/Lottery/Classes/Discover(发现)/Controller/WXDiscoverViewController.m
+++ Lottery/Lottery/Classes/Discover(发现)/Controller/WXDiscoverViewController.m
@@ -17,4 +17,26 @@
     return [storyBoard instantiateInitialViewController];
 }
 
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    // 即将显示时刷新列表
+    [self.tableView reloadData];
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // 先移除屏幕左侧
+    cell.transform = CGAffineTransformTranslate(cell.transform, screenW, 0);
+    
+    // 动画移动回来
+    [UIView animateWithDuration:0.5 animations:^{
+        cell.transform = CGAffineTransformIdentity;
+    }];
+    
+    
+}
+
 @end
